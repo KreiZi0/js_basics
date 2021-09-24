b089cb57d85816113609c80f02d44a1360597327
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-09-24 11:17:43 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("TERE TERE VANAKERE.");
+// Muutujad 
+/*
+vaatame
+kuidas
+luua ja kasutada muutujad
+*/ 
+//Muutuja, mille sisu saab muuta
+
+let name = "Steven Mirontšuk";
+
+name = "Steven Lepp";
+
+
+let eesnimi = "Steven";
+
+//Muutuja, mis on konstantne
+const perenimi = "Mirontšuk";
+// perenimi = "Lepp"
+const numbers = [1, 2, 3, 4];
+numbers.push(6);
+
+const person = {
+	name: "Steven",
+	age: 18
+};
+
+person.age = 19
+console.log(person);
+console.log(numbers);
