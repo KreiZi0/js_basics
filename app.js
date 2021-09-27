0cebc21f111a319d46677ed7893c17b0b9e7ecb5
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-09-27 15:14:59 +0300

    Switch

--- app.js
+++ app.js
@@ -1,75 +1,49 @@
-/*
-if (tingimus){
-	//tegevused if puhul
-} else {
-	// tegevused else puhul
-}
-*/
+let color = "blue";
 
-const id = 100;
+switch(color){
+	case 'red':
+	console.log('stop!');
+	break;
+	case 'yellow':
+	console.log("Attention");
+	break;
+	case 'green':
+	console.log("Go!");
+	break;
+	default:
+	console.log("This color is not allowed");
+	break;
 
-// võrdub  - ==
-// võrdub + tüübikontroll - ===
-// ei võrdu - !=
-// ei võrdu + tüübikontroll - !==
-
-
-if(id !== 100){
-	console.log("OK")
-
-} else {
-	console.log("NOT OK")
 }
 
-let testId;
+let kuupäev = new Date();
+let päevanr = kuupäev.getDay();
+//console.log(päevanr);
 
-if(testId !== "undefined"){
-	console.log("id is ok");
+switch(päevanr){
+	case 1:
+	console.log('esmaspäev');
+	break;
+	case 2:
+	console.log('teisipäev');
+	break;
+	case 3:
+	console.log('kolmapäev');
+	break;
+	case 4:
+	console.log('neljapäev');
+	break;
+	case 5:
+	console.log("reede");
+	break;
+	case 6:
+	console.log("laupäev");
+	break;
+	case 7:
+	console.log("pühapäev");
+	break;
+	default:
+	console.log("Ei sobi");
+	break;
 
-}	else {
-	console.log("id does not exist");
-}
-
-// > ja <
-
-if(testId > 100 ){
-	console.log("id is greater than 1000");
-
-}	else {
-	console.log("id is equal 100 and less that");
-}
-
-
-// if - else if - else
-//let color = 'blue';
-
-//if(color === 'red'){
-	//console.log("Stop!");
-
-
-let name = 'Kadi';
-let age = 8;
-
-if (age > 0 && age <= 12){
-	console.log(`${name} on laps`);
-} else if (age > 12 && age < 18){
-	console.log(`${name} on nooruk` );
-}	else {
-	console.log(`${name} on täiskasvanu`);
-}
-
-
-if (age < 18 || age > 65){
-	console.log(`${name} ei saa registreerida`);
-}	else {
-	console.log(`${name} saab registreerida`);
-}
-
-// ? - ternary operator
-console.log(id == 100 ? 'OK' : 'NOT OK')
-
-if (id===10){
-	console.log('ÒK')
-}	else{
-	console.log("NOT");
 }
