9924210da5033a2c0abed716a2c5504d7218906f
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-09-27 14:51:52 +0300

    Conditions

--- app.js
+++ app.js
@@ -1,24 +1,75 @@
-const today = new Date();
+/*
+if (tingimus){
+	//tegevused if puhul
+} else {
+	// tegevused else puhul
+}
+*/
 
-val = today.getMonth(); // jaanuar - 0
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+const id = 100;
+
+// võrdub  - ==
+// võrdub + tüübikontroll - ===
+// ei võrdu - !=
+// ei võrdu + tüübikontroll - !==
 
 
-birthday = new Date('10/02/2021 12:00:50');
+if(id !== 100){
+	console.log("OK")
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(2020);
-birthday.setHours(17);
-birthday.setMinutes(30);
-birthday.setSeconds(40);
-birthday.setMilliseconds(15);
+} else {
+	console.log("NOT OK")
+}
 
-console.log(birthday);
+let testId;
+
+if(testId !== "undefined"){
+	console.log("id is ok");
+
+}	else {
+	console.log("id does not exist");
+}
+
+// > ja <
+
+if(testId > 100 ){
+	console.log("id is greater than 1000");
+
+}	else {
+	console.log("id is equal 100 and less that");
+}
+
+
+// if - else if - else
+//let color = 'blue';
+
+//if(color === 'red'){
+	//console.log("Stop!");
+
+
+let name = 'Kadi';
+let age = 8;
+
+if (age > 0 && age <= 12){
+	console.log(`${name} on laps`);
+} else if (age > 12 && age < 18){
+	console.log(`${name} on nooruk` );
+}	else {
+	console.log(`${name} on täiskasvanu`);
+}
+
+
+if (age < 18 || age > 65){
+	console.log(`${name} ei saa registreerida`);
+}	else {
+	console.log(`${name} saab registreerida`);
+}
+
+// ? - ternary operator
+console.log(id == 100 ? 'OK' : 'NOT OK')
+
+if (id===10){
+	console.log('ÒK')
+}	else{
+	console.log("NOT");
+}
