148f312cacddfca675c5e5afa530325edb32b75f
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-09-27 13:43:58 +0300

    Objects

--- app.js
+++ app.js
@@ -1,52 +1,29 @@
-const numbers = [43, 56, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruits = ['õun', 'banaan', 'virsik'];
 
-const mixed = [22, 'tere', true, undefined, null, {a:1, b:2}, new Date()];
+const person = {
+
+	firstname: "Kadi",
+	lastname: "Tamm",
+	age: 23,
+	email: "kadi.tamm@gmail.com",
+	hobbies: ['muusika', 'sport'],
+	address: {
+		city: 'Tallinn',
+		country: 'Estonia'
+	},
+	getBirthdayYear: function(){
+		return 2021 - this.age;
+	}
+
+
+};
+
 
 let val;
 
-
-val = numbers.lenght; // massiivi pikkus
-
-val = Array.isArray(numbers); // Kas on massiiv
-
-val = numbers[3]; // kindel väärtus
-
-numbers[3] = 100; // asendamine
-
-// lisamine 
-
-numbers.push(250);
-numbers.unshift(120);
-
-//maha võtmine
-numbers.pop();
-numbers.shift();
-
-//index
-val = numbers.indexOf(100);
-
-//lõikamine
-numbers.splice(2, 1);
-
-// ühendamine
-val = numbers.concat(numbers2); 
-
-// sorteerimine kasvujärjekorras
-
-val = numbers.sort(function(x, y) {
-	return x - y;
-}); 
-
-//sorteerimine kahanemisjärjekorras
-
-val = numbers.sort(function(x, y) {
-	return y - x;
-}); 
-
-val = numbers.splice(3, 0, 100);
-
-
-console.log(numbers)
+val = person.firstname;
+val = person['lastname'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address['city'];
+val = person.getBirthdayYear();
 console.log(val);
