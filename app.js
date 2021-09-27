6d93bebae345c4f1b56abfb593868b1bf9002c06
KreiZi0 <steven.mirontsuk1@gmail.com>
2021-09-27 13:27:01 +0300

    Arrays and methods

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
-const firstname = "Kadi";
-const surname = "Tamm";
-const age = 20;
-const text = "Tere, olen Kadi!";
-const tags = "arendus, veeb";
+const numbers = [43, 56, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruits = ['õun', 'banaan', 'virsik'];
+
+const mixed = [22, 'tere', true, undefined, null, {a:1, b:2}, new Date()];
 
 let val;
 
-//concatenation - es5
-val = "<ul>" 
-"<li>"
 
-	"</ul>"
+val = numbers.lenght; // massiivi pikkus
 
-//templating - es6
-val = `
-	<ul>
-		<li>Eesnimi: ${firstname} </li>
-		<li>Perenimi: ${surname}</li>
-		<li>Vanus:  ${age}</li>
-		<li>Alad: ${tags}</li>
-	</ul>
-`;
+val = Array.isArray(numbers); // Kas on massiiv
 
+val = numbers[3]; // kindel väärtus
+
+numbers[3] = 100; // asendamine
+
+// lisamine 
+
+numbers.push(250);
+numbers.unshift(120);
+
+//maha võtmine
+numbers.pop();
+numbers.shift();
+
+//index
+val = numbers.indexOf(100);
+
+//lõikamine
+numbers.splice(2, 1);
+
+// ühendamine
+val = numbers.concat(numbers2); 
+
+// sorteerimine kasvujärjekorras
+
+val = numbers.sort(function(x, y) {
+	return x - y;
+}); 
+
+//sorteerimine kahanemisjärjekorras
+
+val = numbers.sort(function(x, y) {
+	return y - x;
+}); 
+
+val = numbers.splice(3, 0, 100);
+
+
+console.log(numbers)
 console.log(val);
-document.body.innerHTML = val;
